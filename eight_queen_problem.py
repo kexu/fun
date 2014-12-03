4a8b13e127ba3c7fea661495b0d4a634d77c9473
kexu <kelvin.k.xu@gmail.com>
2014-12-03 08:18:21 -0800

    File created

---
+++ eight_queen_problem.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# File          : eight_queen_problem.py
+# Description   : solve 8 queen problem 
+# Author        : Ke Xu, x.u.k.e@qq.com
+# History        
+# 1.0 2014/12/03: initial version
+# -----------------------------------------------------------------------------
+
+

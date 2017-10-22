9d19dd570d78b9fa759e6afa5ebd2d78df00cba4
KEXU <kkexu@outlook.com>
2017-10-22 06:23:59 -0700

    Update source code

--- calc_pi.py
+++ calc_pi.py
@@ -6,5 +6,16 @@
 # 1.0 2017/10/22: initial version
 # -----------------------------------------------------------------------------
 
+from random import random
+def pi(n):
+    t = 0
+    for kk in range(n):
+        x = random() ** 2
+        y = random() ** 2
+        if x + y <= 1:
+            t += 1
+    return t/n*4
+
+print (pi(10**7))
 
 

0754dd15e403ba8626c91191117548f7b4da61cc
KEXU <kkexu@outlook.com>
2017-10-22 06:08:11 -0700

    add initial file

---
+++ calc_pi.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------------------------------------
+# File          : calc_pi.py
+# Description   : calculate the value of PI by Monte Carlo simulation 
+# Author        : Ke Xu, kkexu@outlook.com
+# History        
+# 1.0 2017/10/22: initial version
+# -----------------------------------------------------------------------------
+
+
+

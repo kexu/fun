86b1ed9242182a707fd1603ee18f1679fdb94735
kexu <kkexu@outlook.com>
2018-03-20 23:15:06 -0700

    Git commit test

--- convol/convol.py
+++ convol/convol.py
@@ -6,5 +6,8 @@
 # 1.0 03/08/2018: initial version
 # -----------------------------------------------------------------------------
 
+import numpy as np
+import cv2
 
-
+img = cv2.imread('traffic.jpg', 3)
+print img
